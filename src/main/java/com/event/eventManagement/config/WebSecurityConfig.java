6f765e2b207a886992657f43bc9c842e6e7626af
Vinayak <153752985+Vinayakmangalagi@users.noreply.github.com>
2025-01-26 21:45:04 +0530

    security

--- src/main/java/com/event/eventManagement/config/WebSecurityConfig.java
+++ src/main/java/com/event/eventManagement/config/WebSecurityConfig.java
@@ -1,14 +1,25 @@
 package com.event.eventManagement.config;
 
+import com.event.eventManagement.security.CustomerUserDetailsService;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class WebSecurityConfig {
+    @Autowired
+    private CustomerUserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -18,5 +29,21 @@ public class WebSecurityConfig {
                                         .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                                                 .httpBasic(Customizer.withDefaults())
                 .build();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ src/main/java/com/event/eventManagement/controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.event.eventManagement.controller;
+
+import com.event.eventManagement.entity.AuthModel;
+import com.event.eventManagement.entity.User;
+import com.event.eventManagement.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody AuthModel authModel){
+      authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authModel.getEmail(),authModel.getPassword()));
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register (@RequestBody User user){
+        return new ResponseEntity<>(userService.createUser(user),HttpStatus.CREATED);
+    }
+}
